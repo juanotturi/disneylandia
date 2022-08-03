e22bb21776e136eb61ce49ddaf21f2c8eed54c96
juanotturi <juanotturi@gmail.com>
2022-08-03 14:54:53 -0300

    commit inicial

---
+++ src/main/java/com/alkemy/disneylandia/disneylandia/DisneylandiaApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.disneylandia.disneylandia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneylandiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneylandiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/disneylandia/disneylandia/DisneylandiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.disneylandia.disneylandia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneylandiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
