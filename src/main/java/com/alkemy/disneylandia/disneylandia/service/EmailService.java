7291e755028110552fe85504d34147bd7de8c0b3
juanotturi <juanotturi@gmail.com>
2022-08-19 16:38:15 -0300

    Cambios

--- src/main/java/com/alkemy/disneylandia/disneylandia/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.alkemy.disneylandia.disneylandia.service;
-
-import java.io.IOException;
-
-public interface EmailService {
-    void sendWelcomeEmailTo(String to) throws IOException;
-}
